4cf54d43c93265c2d8effcf54e22084148f84c4f
SIAS8848 <1776899033@qq.com>
2025-05-26 21:47:17 +0800

    实现简单时钟

--- app/src/main/java/com/example/helloandroid/MainActivity.kt
+++ app/src/main/java/com/example/helloandroid/MainActivity.kt
@@ -2,46 +2,16 @@ package com.example.helloandroid
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.helloandroid.ui.theme.HelloAndroidTheme
+
+import com.example.helloandroid.customizeview.CustomizeView
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContent {
-            HelloAndroidTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
-        }
-    }
-}
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+        val customizeView = CustomizeView(this)
+        setContentView(customizeView) // 直接将它作为根视图
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    HelloAndroidTheme {
-        Greeting("Android")
     }
 }
